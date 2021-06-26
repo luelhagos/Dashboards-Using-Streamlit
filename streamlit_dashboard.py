6c263eb2fbff59ccdac9864620639c84460b23f3
Luel Hagos <luelh0101@gmail.com>
2021-06-26 12:02:58 +0300

    add function names

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -9,3 +9,36 @@ from add_data import db_execute_fetch
 st.title('Topic Modeling and Sentiment Analysis')
 
 st.write("## Topic modeling and sentimet anlysis of twiteer data")
+
+
+# loads the data fromm the db based on query
+def loadData():
+    query = "select * from Tweet"
+    df = db_execute_fetch(query, dbName="db_tweets", rdf=True)
+    return df
+
+
+def wordCloud():
+  """
+  To be implemented..
+  """
+  pass
+  
+def barChart():
+  """
+  To be implemented..
+  """
+  pass
+  
+def pieChart():
+  """
+  To be implemented..
+  """
+  pass
+    
+def selectPolarity():
+  """
+  To be implemented..
+  """
+  pass
+  
