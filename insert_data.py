d7032a7688c98a400aa0aeec456b2f8ae0ca3cfd
Luel Hagos <luelh0101@gmail.com>
2021-06-28 00:18:30 +0300

    insertion to the table updated

--- insert_data.py
+++ insert_data.py
@@ -6,17 +6,6 @@ import mysql.connector as mysql
 from mysql.connector import Error
 
 def DBConnect(dbName=None):
-    """
-
-    Parameters
-    ----------
-    dbName :
-        Default value = None)
-
-    Returns
-    -------
-
-    """
     conn = mysql.connect(host='localhost', user='root', password='pass@1221',
                          database=dbName, buffered=True)
     cur = conn.cursor()
@@ -29,42 +18,12 @@ def emojiDB(dbName: str) -> None:
     conn.commit()
 
 def createDB(dbName: str) -> None:
-    """
-
-    Parameters
-    ----------
-    dbName :
-        str:
-    dbName :
-        str:
-    dbName:str :
-
-
-    Returns
-    -------
-
-    """
     conn, cur = DBConnect()
     cur.execute(f"CREATE DATABASE IF NOT EXISTS {dbName};")
     conn.commit()
     cur.close()
 
 def createTables(dbName: str) -> None:
-    """
-
-    Parameters
-    ----------
-    dbName :
-        str:
-    dbName :
-        str:
-    dbName:str :
-
-
-    Returns
-    -------
-
-    """
     conn, cur = DBConnect(dbName)
     sqlFile = 'sql_schema.sql'
     fd = open(sqlFile, 'r')
@@ -85,27 +44,11 @@ def createTables(dbName: str) -> None:
     return
 
 def preprocess_df(df: pd.DataFrame) -> pd.DataFrame:
-    """
-
-    Parameters
-    ----------
-    df :
-        pd.DataFrame:
-    df :
-        pd.DataFrame:
-    df:pd.DataFrame :
-
-
-    Returns
-    -------
-
-    """
 
     df.rename({'original_text':'clean_text'}, axis=1, inplace=True) # rename 'original_text' with 'clean_text'
-    df = df[['clean_text', 'polarity']] # select only columns with 'clean_text', 'polarity' and update the df
-
+    df = df.drop(columns=['possibly_sensitive'], axis=1)
     # Remove hyperlinks
-    rgx = lambda x: re.sub('http[s]?://\S+', '', x)
+    rgx = lambda x: re.sub('http[s]?', '', x)
     df['clean_text'] = df['clean_text'].map(rgx)
 
     # Remove punctuation
@@ -113,47 +56,22 @@ def preprocess_df(df: pd.DataFrame) -> pd.DataFrame:
     df['clean_text'].apply(lambda x: x.translate(str.maketrans(' ', ' ', string.punctuation)))
 
     df['polarity'] = pd.to_numeric(df['polarity'],errors='coerce')  # change polarity to numeric
-    df.dropna() # remove rows and columns with Null/NaN values.
+    df=df.dropna() # remove rows and columns with Null/NaN values.
     return df
 
 
 def insert_to_tweet_table(dbName: str, df: pd.DataFrame, table_name: str) -> None:
-    """
-
-    Parameters
-    ----------
-    dbName :
-        str:
-    df :
-        pd.DataFrame:
-    table_name :
-        str:
-    dbName :
-        str:
-    df :
-        pd.DataFrame:
-    table_name :
-        str:
-    dbName:str :
-
-    df:pd.DataFrame :
-
-    table_name:str :
-
-
-    Returns
-    -------
-
-    """
+        
     conn, cur = DBConnect(dbName)
 
     df = preprocess_df(df)
     
-
     for _, row in df.iterrows():
-        sqlQuery = f"""INSERT INTO {table_name} (clean_text, polarity) 
-        VALUES(%s, %s);"""
-        data = (row[0], row[1])
+        sqlQuery = f"""INSERT INTO {table_name} (created_at, source, clean_text, polarity, subjectivity,
+       lang, favorite_count, retweet_count, original_author, followers_count, friends_count, hashtags, user_mentions, place)
+        VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+        data = (row[0], row[1], row[2], row[3], (row[4]), (row[5]), row[6], row[7], row[8], row[9], row[10], row[11],
+                row[12], row[13])
 
         try:
             # Execute the SQL command
@@ -167,25 +85,6 @@ def insert_to_tweet_table(dbName: str, df: pd.DataFrame, table_name: str) -> Non
     return
 
 def db_execute_fetch(*args, many=False, tablename='', rdf=True, **kwargs) -> pd.DataFrame:
-    """
-
-    Parameters
-    ----------
-    *args :
-
-    many :
-         (Default value = False)
-    tablename :
-         (Default value = '')
-    rdf :
-         (Default value = True)
-    **kwargs :
-
-
-    Returns
-    -------
-
-    """
     connection, cursor1 = DBConnect(**kwargs)
     if many:
         cursor1.executemany(*args)
@@ -214,11 +113,11 @@ def db_execute_fetch(*args, many=False, tablename='', rdf=True, **kwargs) -> pd.
 
 
 if __name__ == "__main__":
-    createDB(dbName='db_tweets')
-    emojiDB(dbName='db_tweets')
-    createTables(dbName='db_tweets')
+    createDB(dbName='Tweetdb')
+    emojiDB(dbName='Tweetdb')
+    createTables(dbName='Tweetdb')
 
     df = pd.read_csv('processed_tweet_data.csv')
 
-    insert_to_tweet_table(dbName='db_tweets', df=df, table_name='Tweet')
+    insert_to_tweet_table(dbName='Tweetdb', df=df, table_name='Tweet')
     
