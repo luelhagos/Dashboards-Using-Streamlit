a3b4043a1c471758d833e7fae6263192a2bc52d6
Luel Hagos <luelh0101@gmail.com>
2021-06-27 06:29:29 +0300

    selectbox for the sidebar added

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -10,12 +10,11 @@ st.title('Topic Modeling and Sentiment Analysis')
 
 st.write("## Topic modeling and sentimet anlysis of twiteer data")
 
-
 # loads the data fromm the db based on query
 def loadData():
     query = "select * from Tweet"
     df = db_execute_fetch(query, dbName="db_tweets", rdf=True)
-    return df
+    return df 
 
 def text_category(p):
     """
@@ -32,24 +31,8 @@ def polarity_count():
     df['score'] = df['polarity'].apply(text_category) 
     sc = list(df['score'])
     return { 'positive': sc.count('positive'), 'neutral': sc.count('neutral'),
-                            'negative': sc.count('negative')  }
+                            'negative': sc.count('negative')  } 
 
-
-def wordCloud():
-    df = loadData()
-    # Convert to lowercase
-    df['clean_text'] = df['clean_text'].map(lambda x: x.lower())
-    # Join the different processed titles together.
-    long_string = ','.join(list(df['clean_text'].values))
-
-        # Create a WordCloud object
-    wordcloud = WordCloud(background_color="black", width=650, height=450, \
-                             min_font_size=5, contour_color='steelblue')
-    # Generate a word cloud
-    wordcloud.generate(long_string)
-    st.title("Tweet Text Word Cloud")
-    st.image(wordcloud.to_array())
-  
 # draws bar chart of the polarity
 def barChart():
     st.title('Bar Chart')
@@ -60,7 +43,7 @@ def barChart():
                 })
     bar_fig = px.bar(data, x='Sentiment', y='Tweets')
     st.plotly_chart(bar_fig)
-  
+
 # draws pie chart of the polarity
 def pieChart():
     st.title('Pie Chart')
@@ -68,9 +51,29 @@ def pieChart():
     pie_fig = px.pie(values=[count[key] for key in count.keys()], names=list(count.keys()))
     st.plotly_chart(pie_fig)
     
-def selectPolarity():
-  """
-  To be implemented..
-  """
-  pass
-  
+
+# topic modeling
+def wordCloud():
+    df = loadData()
+    # Convert to lowercase
+    df['clean_text'] = df['clean_text'].map(lambda x: x.lower())
+    # Join the different processed titles together.
+    long_string = ','.join(list(df['clean_text'].values))
+
+    # Create a WordCloud object
+    wordcloud = WordCloud(background_color="black", width=650, height=450, \
+                             min_font_size=5, contour_color='steelblue')
+    # Generate a word cloud
+    wordcloud.generate(long_string)
+    st.title("Tweet Text Word Cloud")
+    st.image(wordcloud.to_array())
+
+#st.title("Data Visualizations")
+random_tweet = st.sidebar.selectbox('Visualizations', 
+                ('Topic Modeling','Bar Chart','Pie Chart'))
+if random_tweet == 'Topic Modeling':
+    wordCloud()
+elif random_tweet == 'Bar Chart':
+    barChart()
+elif random_tweet == 'Pie Chart':
+    pieChart()
